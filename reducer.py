a523c91a95c9169389fa7fbc1b1ce42bfd10dfe8
ENUKOLUnaga <enkolunagendra@gmail.com>
2026-02-16 16:22:59 +0530

    Adding-custom-mapper-reducer-files

---
+++ reducer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+
+current_word = None
+current_count = 0
+
+# Read input from stdin
+for line in sys.stdin:
+    line = line.strip()
+    word, count = line.split("\t", 1)
+    count = int(count)
+
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print(f"{current_word}\t{current_count}")
+        current_word = word
+        current_count = count
+
+# Print last word
+if current_word == word:
+    print(f"{current_word}\t{current_count}")
